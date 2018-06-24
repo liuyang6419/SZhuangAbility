49ac5a8514676dc3839facdca4ce910cc001ffe4
liuyang6419 <303308260@qq.com>
2018-06-24 10:18:41 +0100

    add autoencode model

---
+++ auto_encoder.py
@@ -0,0 +1,139 @@
+from keras.layers import Input, Dense
+from keras.models import Model, load_model
+import numpy as np
+from glob import glob
+import pickle as pickle
+from keras.preprocessing.image import img_to_array, load_img
+from lshash import LSHash
+from keras import backend as K
+from gevent.pool import Pool
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def generate_arrays_from_file():
+    '''
+    读取图片
+    :return:
+    '''
+    files = glob("./data/dataset/*.jpg")
+    files = files
+    x = []
+    pool = Pool(size=100)
+    res = pool.imap(lambda filename: img_to_array(load_img(filename)) / 255, files)
+    for img_area in res:
+        x.append(img_area)
+    x = np.array(x)
+    return x
+
+
+def get_train_img_data(save_path="data/train.npy"):
+    '''
+    图片数据保存成np.array
+    :param save_path:
+    :return:
+    '''
+    train_data = generate_arrays_from_file()
+    np.save(save_path, train_data)
+
+
+def build_auto_encode_model(shape=[48 * 64 * 3], encoding_dim=100):
+    '''
+    建立自编码神经网络模型
+    :param shape:
+    :param encoding_dim:
+    :return:
+    '''
+    input_img = Input(shape=shape)
+    encoded = Dense(encoding_dim, activation='relu')(input_img)
+    out_shape = np.product(shape)
+    decoded = Dense(out_shape, activation='sigmoid')(encoded)
+    autoencoder = Model(input=input_img, outputs=decoded)
+    encoder = Model(inputs=input_img, outputs=encoded)
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    return encoder, autoencoder
+
+
+def train_auto_encode_model(encoder_model_path="./data/encoder.h5"):
+    '''
+    图片数据训练自编码神经网络
+    :param encoder_model_path:
+    :return:
+    '''
+    X = np.load("data/train.npy")
+    X = X.reshape(X.shape[0], -1)
+    X_train = X[int(round(X.shape[0] * 0.2)):, :]
+    X_test = X[0:int(round(X.shape[0] * 0.2)), :]
+    encoder, autoencoder = build_auto_encode_model()
+    autoencoder.fit(X_train, X_train, epochs=10, batch_size=256, shuffle=True, validation_data=(X_test, X_test))
+    encoder.save(encoder_model_path)
+
+
+def index_room():
+    '''
+    lsh算法索引图片特征
+    :return:
+    '''
+    files = glob("./data/features/*.csv")
+    files_ids = [filename.split("/")[-1].replace(".csv", "") for filename in files]
+    print(files_ids)
+    X = np.load("data/train.npy")
+    X = X.reshape(X.shape[0], -1)
+    print(X)
+    encoder = load_model("data/encoder.h5")
+    dimension = 100
+    print(dimension)
+    lsh_hash = LSHash(hash_size=32, input_dim=dimension)
+    print(lsh_hash)
+    compress_feature = encoder.predict(X)
+    print(compress_feature)
+    for num, ele in enumerate(compress_feature.tolist()):
+        lsh_hash.index(ele, extra_data=files_ids[num])
+    with open("data/lsh.pkl", "wb") as fh:
+        pickle.dump(lsh_hash, fh)
+
+
+def get_picture_feature(picture_path):
+    '''
+    根据自编码神经网络算出图片的100维特征
+    :param picture_path:
+    :return:
+    '''
+    img_area = img_to_array(load_img(picture_path)) / 255.0
+    img_area = np.array([img_area])
+    img_area = img_area.reshape(img_area.shape[0], -1)
+    encoder = load_model("data/encoder.h5")
+    compress_feature = encoder.predict(img_area)
+    return compress_feature.tolist()[0]
+
+
+def get_similar_room_example(pic_path = r"data/dataset/7480.jpg"):
+    with open("data/lsh.pkl", "rb") as fh:
+        img_list = []
+        lsh = pickle.load(fh)
+        res = get_picture_feature(pic_path)
+        res = lsh.query(res, num_results=10) # 查询前10个最相似的图片
+        print("query room id is {}".format(pic_path))
+        for ele in res:
+            data_id = ele[0][1]
+            distance = ele[1]
+            print("similar room id is {0} and the distance is distance {1}\n".format(data_id, distance))
+            img_list.append(data_id)
+        show_similar_images(img_list)
+
+def show_similar_images(list):
+    for i in range(10):
+        im = Image.open("./data/images/" + str(list[i])+ ".jpg")
+        im.show()
+
+
+if __name__ == '__main__':
+    # get_train_img_data() #读取图Wx`片数据
+    # print("Finish loading img data")
+    # train_auto_encode_model() #训练模型
+    # print("Finished train_model")
+    # index_room() # 给降维的图片数据建立索引
+    # print("Finished indexing")
+
+    get_similar_room_example() #检索相似房间
+    # K.clear_session()
